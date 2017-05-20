1f5b044e712a6d344439c154c498a5eae6a2a2b2
Malko18 <mf64849@gmail.com>
2017-05-19 17:53:41 -0700

    new commit

--- src/Profile.java
+++ src/Profile.java
@@ -1,8 +1,15 @@
 //profile class
 public class Profile implements Comparable<Profile>{
+
     private String nama,email;
+
+    public Profile(String nama,String email){
+        this.nama = nama;
+        this.email = email;
+    }
+
     @Override
-    public int compareTo(Profile o) {
+    public int compareTo(Profile p) {
         return 0;
     }
 }
