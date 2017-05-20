f05150077f566644846b34f8e7be192811242598
Malko18 <mf64849@gmail.com>
2017-05-19 18:28:36 -0700

    new commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 //main class
 public class Main {
+
     public static void main(String[] args){
-        
+
+        ArrayList<Profile> profiles = new ArrayList<>();
+        ArrayList<Circle> circles = new ArrayList<>();
+        HashMap<String, Circle> circleHashMap = new HashMap<>();
     }
+
 }
