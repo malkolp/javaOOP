53bc82c842b68865bcd8631f2c97e717709e89a2
Malko18 <mf64849@gmail.com>
2017-05-19 18:24:46 -0700

    new commit

--- src/Circle.java
+++ src/Circle.java
@@ -1,3 +1,49 @@
 //circle class
-public class Circle {
+public class Circle implements Comparable<Circle> {
+
+    private Profile profile,profile1,profile2,profile3,profile4,profile5,profile6;
+
+    public Circle(Profile profile, Profile profile1, Profile profile2, Profile profile3, Profile profile4, Profile profile5, Profile profile6){
+        this.profile = profile;
+        this.profile1 = profile1;
+        this.profile2 = profile2;
+        this.profile3 = profile3;
+        this.profile4 = profile4;
+        this.profile5 = profile5;
+        this.profile6 = profile6;
+    }
+
+    public Profile getProfile() {
+        return profile;
+    }
+
+    public Profile getProfile1() {
+        return profile1;
+    }
+
+    public Profile getProfile2() {
+        return profile2;
+    }
+
+    public Profile getProfile3() {
+        return profile3;
+    }
+
+    public Profile getProfile4() {
+        return profile4;
+    }
+
+    public Profile getProfile5() {
+        return profile5;
+    }
+
+    public Profile getProfile6() {
+        return profile6;
+    }
+    
+
+    @Override
+    public int compareTo(Circle c) {
+        return 0;
+    }
 }
